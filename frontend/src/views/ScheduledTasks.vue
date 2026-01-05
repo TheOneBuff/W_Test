20b414385d45c3433d706aaff73d81692127b5bd
whp <1148703191@qq.com>
2026-01-05 20:26:19 +0800

    修改定时任务 任务状态多了title

--- frontend/src/views/ScheduledTasks.vue
+++ frontend/src/views/ScheduledTasks.vue
@@ -39,7 +39,6 @@
           </template>
         </el-table-column>
         <el-table-column label="状态" width="100">
-          <el-table-column label="状态" width="100">
           <template #default="{ row }">
             <el-switch
               v-model="row.is_enabled"
@@ -47,7 +46,6 @@
               :loading="row._switching"
             />
           </template>
-        </el-table-column>
         </el-table-column>
 
         <el-table-column label="操作" width="250" fixed="right">
